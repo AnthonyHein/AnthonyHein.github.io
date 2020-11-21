645117be8d1af821696abd62dd87b5937635e0c5
Anthony Hein <anhein@princeton.edu>
2020-11-21 09:16:10 -0500

    New build.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,19 +15,13 @@ export default class Home extends React.Component {
             <Jumbotron>
                 <Container>
                     <h1 className={"text-center display-4"}>Daily Princetonian 145th Managing Board Elections</h1>
-                    <p className={"lead text-center"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at
-                        commodo ipsum. Sed ullamcorper ligula et malesuada efficitur.</p>
+                    <p className={"lead text-center"}>
+                    Thank you for participating in The Daily Princetonian’s 2020 Editor-in-Chief election!
+                    </p>
                     <hr/>
-                    <p className={"intro-p"}>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
-                        cubilia curae;
-                        Vestibulum mi nulla, gravida ac mi sed, tincidunt feugiat sapien. Morbi elementum felis nec
-                        ullamcorper laoreet. Praesent efficitur lobortis orci feugiat pretium. Donec maximus dictum
-                        tellus. Curabitur sollicitudin nisl a sapien tempor, ut euismod nibh feugiat. Donec non
-                        efficitur nisl. Vestibulum congue libero nec mollis laoreet. Aliquam dictum arcu tellus,
-                        malesuada hendrerit sapien placerat eget. Duis faucibus vel sapien at feugiat. Donec nunc orci,
-                        gravida non sapien nec, lacinia dapibus sem. Nam fermentum fringilla mi id tincidunt.
-                        Pellentesque hendrerit, nulla in finibus lobortis, risus ligula iaculis enim, ut feugiat massa
-                        nunc vel arcu. Vestibulum eu fermentum dui, at congue felis. Duis faucibus felis tellus.
+                    <p className={"intro-p"}>
+                    On this site, you’ll find your personal ballot, as well as each candidate’s platform and pre-recorded speech.
+                    If you have any questions or encounter technical difficulties, please contact Jon at <a href="mailto:jaort@princeton.edu">jaort@princeton.edu</a>.
                     </p>
 
                     <Button variant={"orange"} className={"text-center"} href={"#/vote"}>Vote Now!</Button>{' '}
